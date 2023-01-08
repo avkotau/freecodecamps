633b1e69feef33f1d51bd49e0dd86fa4af48e2de
avkotau <kotov.alexander1@mail.ru>
2023-01-08 13:37:24 +0100

    tasks 08.01.22

---
+++ 58.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "Gamer",
+    legs: 4,
+    tails: 0.5,
+    friends: ["toy", "bone"],
+
+    // Only change code above this line
+};
