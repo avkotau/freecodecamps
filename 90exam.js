22df92ac80df7f068911e9ba7dc7caf06e833952
avkotau <kotov.alexander1@mail.ru>
2023-01-16 18:06:26 +0100

    tasks 16.01.22

---
+++ 90exam.js
@@ -0,0 +1,34 @@
+// Only change code below this line
+
+function countdown(n) {
+
+    if (n < 1) {
+        return [];
+    } else {
+        let result = countdown(n - 1);
+        result.splice(0, 0, n);
+        return result;
+    }
+}
+
+// or
+//function countdown(n) {
+//   if (n < 1) {
+//     return [];
+//   } else {
+//     const arr = countdown(n - 1);
+//     arr.splice(0, 0, n);
+//     return arr;
+//   }
+// }
+//or
+//function countdown(n){
+//    return n < 1 ? [] : [n].concat(countdown(n - 1));
+// }
+//or
+//function countdown(n){
+//    return n < 1 ? [] : [n, ...countdown(n - 1)];
+// }
+countdown(5) //[5, 4, 3, 2, 1]
+countdown(10) //[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+// Only change code above this line
