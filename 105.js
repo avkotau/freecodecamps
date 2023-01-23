132092a78cbf79a17c4a5db5fed001b3fc4b84f9
avkotau <kotov.alexander1@mail.ru>
2023-01-23 14:23:05 +0100

    tasks 23.01.22

---
+++ 105.js
@@ -0,0 +1,8 @@
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [,,...shorterList] = list;
+    console.log(shorterList);
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
