c9e4f0d0de445f8931499baf7cebd978c6f16fa1
avkotau <kotov.alexander1@mail.ru>
2023-01-27 14:39:51 +0100

    tasks 27.01.22

--- 111.js
+++ 111.js
@@ -1,21 +1,45 @@
 // Only change code below this line
 class Thermostat {
     constructor(temp) {
-        console.log(this._temp = temp)
+        this._temp = temp
     }
-    getter () {
-        console.log(this._temp)
+
+    get temperature () {
+        return this._temp = 5/9 * (this._temp - 32)
     }
-    setter (temperature) {
-        console.log(this._temp = temperature)
+
+    set temperature (t) {
+        this._temp = t  * 9.0 / 5 + 32
     }
+
 }
 // Only change code above this line
 
+
 const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+console.log(thermos)
 let temp = thermos.temperature; // 24.44 in Celsius
+console.log(temp)
 thermos.temperature = 26;
-console.log(thermos.temperature)
 temp = thermos.temperature; // 26 in Celsius
 console.log(temp)
+
+
+// class Book {
+//     constructor(author) {
+//         this._author = author;
+//     }
+//     // getter
+//     get writer() {
+//         return this._author;
+//     }
+//     // setter
+//     set writer(updatedAuthor) {
+//         this._author = updatedAuthor;
+//     }
+// }
+// const novel = new Book('anonymous');
+// console.log(novel.writer);
+// novel.writer = 'newAuthor';
+// console.log(novel.writer);
 
