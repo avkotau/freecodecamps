46046f8864e67feda2da4272b52fc7b398d802ef
avkotau <kotov.alexander1@mail.ru>
2023-02-02 12:00:48 +0100

    tasks 02.02.22

---
+++ 143.js
@@ -0,0 +1,10 @@
+// let username = "JackOfAllTrades";
+let username = "JACK";
+// let username = "Jo";
+// let username = "Oceans11";
+// let username = "RegexGuru";
+// let username = "Z97";
+// let username = "AB1";
+let userCheck = /^A-z+?\w/; // Change this line
+let result = userCheck.test(username);
+console.log(result)
