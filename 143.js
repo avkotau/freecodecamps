63aa929452525aa95e60401f6ecc4e8918747615
avkotau <kotov.alexander1@mail.ru>
2023-02-03 08:31:18 +0100

    tasks 03.02.22

--- 143.js
+++ 143.js
@@ -6,5 +6,6 @@ let username = "JACK";
 // let username = "Z97";
 // let username = "AB1";
 let userCheck = /^A-z+?\w/; // Change this line
+
 let result = userCheck.test(username);
 console.log(result)
