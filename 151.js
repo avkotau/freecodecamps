800c9912251329fbddd0b86273c69002a29ea4d1
avkotau <kotov.alexander1@mail.ru>
2023-02-04 13:02:56 +0100

    tasks 04.02.22

--- 151.js
+++ 151.js
@@ -3,4 +3,3 @@ let myRegex = /El(eanor\sRooseve)lt|Fran(klin .. Roos)evelt/; // Change this lin
 let result = myRegex.test(myString); // Change this line
 // After passing the challenge experiment with myString and see how the grouping works
 console.log(result)
-
