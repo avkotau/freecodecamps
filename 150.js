200b6b4a351d3bf3f2f6f997a6257f8463cdbdc1
avkotau <kotov.alexander1@mail.ru>
2023-02-05 15:24:38 +0100

    tasks 05.02.22

--- 150.js
+++ 150.js
@@ -1,4 +1,9 @@
 let sampleWord = "astronaut";
 let pwRegex = /\w{4,}(?=)(?=[0-9]{2,})/; // Change this line
 let result = pwRegex.test(sampleWord);
+
+//or
+//let sampleWord = "astronaut";
+// let pwRegex =  /(?=\w{6})(?=\w*\d{2})/;
+// let result = pwRegex.test(sampleWord);
 console.log(result)
