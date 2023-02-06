8245189612785f05a109715668a2d5226f7e8cab
avkotau <kotov.alexander1@mail.ru>
2023-02-06 17:51:09 +0100

    tasks 06.02.22

---
+++ 153.js
@@ -0,0 +1,7 @@
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
+
+console.log(str.match(fixRegex))
+console.log(result)
