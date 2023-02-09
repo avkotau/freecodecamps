d4f2d80456a78995a0ef51802fac51955baa4644
avkotau <kotov.alexander1@mail.ru>
2023-02-09 18:16:20 +0100

    tasks 09.02.23

---
+++ 175.js
@@ -0,0 +1,7 @@
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning', ...fragment, 'is', 'fun']; // Change this line
+    console.log(sentence) ;
+}
+
+console.log(spreadOut());
