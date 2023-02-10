56fa1304e432038fb9e6318f8c28706f91db8152
avkotau <kotov.alexander1@mail.ru>
2023-02-10 20:20:47 +0100

    tasks 10.02.23

--- 176.js
+++ 176.js
@@ -1,6 +1,6 @@
 function quickCheck(arr, elem) {
     // Only change code below this line
-    return arr.indexOf(elem) >= 0 ? true : false
+        arr.indexOf(elem) >= 0 ? true : false
     // Only change code above this line
 }
 
