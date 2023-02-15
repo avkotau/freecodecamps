537f6849f82f3dfa9fa4d3e159aa5a79ed77f81f
avkotau <kotov.alexander1@mail.ru>
2023-02-15 13:55:19 +0100

    tasks 15.02.23

---
+++ 192.js
@@ -0,0 +1,7 @@
+function confirmEnding(str, target) {
+   console.log(str.substr(target.length-1, str.length));
+}
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")// false.
+confirmEnding("Bastian", "n") //true
+
+//substring , substr
