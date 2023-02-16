a45b39a19af16683656d66499b5bc24947f5e983
avkotau <kotov.alexander1@mail.ru>
2023-02-16 15:06:44 +0100

    tasks 16.02.23

--- 197.js
+++ 197.js
@@ -2,6 +2,13 @@ function titleCase(str) {
     console.log(str.toLowerCase().split(' ').map((n) => n[0].toUpperCase() + n.slice(1)).join(' '));
 }
 
+//or
+//function titleCase(str) {
+//   return str
+//     .toLowerCase()
+//     .replace(/(^|\s)\S/g, L => L.toUpperCase());
+// }
+
 titleCase("I'm a little tea pot");
 titleCase("I'm a little tea pot")// I'm A Little Tea Pot.
 titleCase("sHoRt AnD sToUt")// Short And Stout.
